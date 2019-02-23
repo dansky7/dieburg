dfaeb1c2abb4f2ef703cd6830295a63cce2c0d18
dansky7 <47505902+dansky7@users.noreply.github.com>
2019-02-23 18:58:14 +0100

    Dieburg.js

---
+++ Dieburg.js
@@ -0,0 +1,20 @@
+var index = 0;
+var x = document.getElementsByTagName("img"); // alle Bilder in Array
+
+for(let i=0; i<x.length; i++){  // alle Bilder ausblenden
+    x[i].style.display = "none";
+}
+x[0].style.display="block"; // erstes Bild einblenden
+carousel();
+
+function carousel(){
+    x[index].style.display="none";
+    index++;
+    if(index<x.length){
+        x[index].style.display = "block";
+    }else{
+        index=0;
+        x[index].style.display = "block"
+    }
+    setTimeout(carousel, 2000);
+}
